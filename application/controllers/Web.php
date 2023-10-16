ac4154b4b75b217e8fd048e47ea1c60e003cca86
abasburhanudin <abashan93@gmail.com>
2023-10-17 04:40:03 +0700

    push pustaka-booking1

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -3,11 +3,20 @@ class Web extends CI_controller
 {
     function _construct()
     {
+        //code...
         parent::_construct();
     }
     public function index()
     {
         $data['judul']="halaman depan";
-        $this->load->view('v_index.php',$data);
+        $this->load->view('v_header',$data);
+        $this->load->view('v_index',$data);
+        $this->load->view('v_footer',$data);
+    }
+    public function about()
+    {$data['judul']="Halaman About";
+    $this->load->view('v_header',$data); 
+    $this->load->view('v_about',$data);
+    $this->load->view('v_footer',$data);  
     }
 }
